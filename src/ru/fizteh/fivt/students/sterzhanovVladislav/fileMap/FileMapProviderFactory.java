0a20c2f0cd66ebffd4430babb171891a24ed45d2
Quadrocube <gliderok@gmail.com>
2013-12-14 13:47:14 +0400

    interface compatibility fix

--- src/ru/fizteh/fivt/students/sterzhanovVladislav/fileMap/FileMapProviderFactory.java
+++ src/ru/fizteh/fivt/students/sterzhanovVladislav/fileMap/FileMapProviderFactory.java
@@ -2,10 +2,13 @@ package ru.fizteh.fivt.students.sterzhanovVladislav.fileMap;
 
 import java.io.IOException;
 
-public class FileMapProviderFactory implements AtomicTableProviderFactory {
+import ru.fizteh.fivt.storage.structured.TableProvider;
+import ru.fizteh.fivt.storage.structured.TableProviderFactory;
+
+public class FileMapProviderFactory implements TableProviderFactory {
 
     @Override
-    public AtomicTableProvider create(String dir) throws IOException {
+    public TableProvider create(String dir) throws IOException {
         return new FileMapProvider(dir);
     }
 
