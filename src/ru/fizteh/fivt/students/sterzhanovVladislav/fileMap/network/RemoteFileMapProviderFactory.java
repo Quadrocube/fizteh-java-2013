c273c16e644e0b81ff8e377ed241335aec269ecf
Quadrocube <gliderok@gmail.com>
2013-12-25 12:46:42 +0400

    factory AutoCloseable

--- src/ru/fizteh/fivt/students/sterzhanovVladislav/fileMap/network/RemoteFileMapProviderFactory.java
+++ src/ru/fizteh/fivt/students/sterzhanovVladislav/fileMap/network/RemoteFileMapProviderFactory.java
@@ -1,16 +1,36 @@
 package ru.fizteh.fivt.students.sterzhanovVladislav.fileMap.network;
 
 import java.io.IOException;
+import java.util.HashSet;
 
 import ru.fizteh.fivt.storage.structured.RemoteTableProvider;
 import ru.fizteh.fivt.storage.structured.RemoteTableProviderFactory;
 
-public class RemoteFileMapProviderFactory implements RemoteTableProviderFactory {
+public class RemoteFileMapProviderFactory implements RemoteTableProviderFactory, AutoCloseable {
 
+    private boolean isClosed = false;
+    private HashSet<RemoteFileMapProvider> providers;
+    
     @Override
-    public RemoteTableProvider connect(String hostname, int port)
-            throws IOException {
-        return new RemoteFileMapProvider(hostname, port);
+    public RemoteTableProvider connect(String hostname, int port) throws IOException {
+        ensureIsOpen();
+        RemoteFileMapProvider provider = new RemoteFileMapProvider(hostname, port);
+        providers.add(provider);
+        return provider;
+    }
+    
+    @Override
+    public void close() throws Exception {
+        isClosed = true;
+        for (RemoteFileMapProvider provider : providers) {
+            provider.close();
+        }
+    }
+    
+    private void ensureIsOpen() {
+        if (isClosed) {
+            throw new IllegalStateException("FileMapProviderFactory was closed");
+        }
     }
 
 }
